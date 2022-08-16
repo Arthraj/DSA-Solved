96afe92525cb2c8e4581e12111db430df2872cab
Arthraj Rathore <64681770+Arthraj@users.noreply.github.com>
2022-08-17 01:06:18 +0530

    Time: 3 ms (58.38%), Space: 7.7 MB (97.83%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,21 @@
+#define pb push_back
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if (n < 2) return n ? nums[0] : 0;
+        return max(robber(nums,0,n-2),robber(nums,1,n-1));
+    }
+    
+//     looted holds true if the last house has been looted
+    int robber(vector<int>& nums, int l, int r) {
+        int pre = 0, cur = 0;
+        for (int i = l; i <= r; i++) {
+            int temp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;
+    }
+};
